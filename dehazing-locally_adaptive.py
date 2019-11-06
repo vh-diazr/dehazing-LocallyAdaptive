8a6911b50e3611f60b1e627e15af101c5ef82942
Victor H. Diaz <vhdiaz@mac-vhdiaz.local>
2019-11-05 17:23:43 -0800

    daily update

--- dehazing-locally_adaptive.py
+++ dehazing-locally_adaptive.py
@@ -5,49 +5,33 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from scipy import *
-#from pylab import *
 import matplotlib.pyplot as plt
-#from scipy.misc import imresize
-#from scipy.misc import imsave
-import cv2
+import numpy as np
+import cv2 
 from cv2.ximgproc import guidedFilter
 from skimage.restoration import denoise_tv_chambolle, denoise_bilateral
 from skimage.io import imread, imsave
 
-#def KNB(mw,cent,K):
-#    r = size(mw)
-#    #cent = mw[int(floor(r/2)), int(floor(c/2))]
-#    dist = zeros(r)
-#    dist = abs(mw - cent)
-#    dist_ord = sort(ravel(dist[:]))
-#    dist_K = dist_ord[K-1]
-#    x = where(dist <= dist_K)
-#    nbh = mw[x]
-#    nbh = ravel(sort(nbh))
-#    nbh = nbh[0:K]
-#    return nbh
-
 def KNB(mw,K):
     mwg = rgb2gray(mw)
-    r = size(mwg)
+    r = np.size(mwg)
     
-    re,co = shape(mwg)
-    nbh = zeros([re,co,3])
+    re,co = np.shape(mwg)
+    nbh = np.zeros([re,co,3])
     #nbh_t = zeros([r,3])
-    nbh_v = zeros([K,3])
-    cent = mwg[int(floor(re/2)), int(floor(co/2))]
-    dist = zeros(r)
+    nbh_v = np.zeros([K,3])
+    cent = mwg[int(np.floor(re/2)), int(np.floor(co/2))]
+    dist = np.zeros(r)
     dist = abs(mwg - cent)
-    dist_ord = sort(ravel(dist[:]))
+    dist_ord = np.sort(np.ravel(dist[:]))
     dist_K = dist_ord[K-1]
-    x,y = where(dist <= dist_K)
+    x,y = np.where(dist <= dist_K)
     nbh[x,y,0] = mw[x,y,0]
     nbh[x,y,1] = mw[x,y,1]
     nbh[x,y,2] = mw[x,y,2]
-    nbh_t0 = sorted(ravel(nbh[:,:,0]),reverse=True)
-    nbh_t1 = sorted(ravel(nbh[:,:,1]),reverse=True)
-    nbh_t2 = sorted(ravel(nbh[:,:,2]),reverse=True)
+    nbh_t0 = sorted(np.ravel(nbh[:,:,0]),reverse=True)
+    nbh_t1 = sorted(np.ravel(nbh[:,:,1]),reverse=True)
+    nbh_t2 = sorted(np.ravel(nbh[:,:,2]),reverse=True)
     nbh_v[:,0] = nbh_t0[0:K]
     nbh_v[:,1] = nbh_t1[0:K]
     nbh_v[:,2] = nbh_t2[0:K]
@@ -63,7 +47,7 @@ def EV(mw,ev):
     return nbh
 
 def rgb2gray(img):
-    img_gray = double( (0.2989*img[:,:,0] + 0.5870*img[:,:,0] + 0.1140*img[:,:,0])) # RGB to Gray Conversion
+    img_gray = np.double( (0.2989*img[:,:,0] + 0.5870*img[:,:,0] + 0.1140*img[:,:,0])) # RGB to Gray Conversion
     return img_gray
         
 S = 19 # Defines the size of sliding-window (SxS) for Airlight estimation
@@ -83,8 +67,8 @@ S = 19 # Defines the size of sliding-window (SxS) for Airlight estimation
 #f_sin = im[:,:,0:3]
 
 ## Read the input hazy image from drive
-imgName = 'fuente'
-f_c = double( imread(imgName +".png") ) # Read Input Hazy Image
+imgName = 'maguey'
+f_c = np.double( imread(imgName +".png") ) # Read Input Hazy Image
 
 ####################
 # Size of the Input Image
@@ -93,16 +77,19 @@ Nr,Nc,Np = f_c.shape
 
 #########################################
 # Extention of the input image to avoid edge issues
-A1 = concatenate((flipud(fliplr(f_c)), flipud(f_c), flipud(fliplr(f_c))), axis=1)
-A2 = concatenate((fliplr(f_c), f_c, fliplr(f_c)), axis=1)
-A3 = concatenate((flipud(fliplr(f_c)), flipud(f_c), flipud(fliplr(f_c))), axis=1)
-f_proc = concatenate( (A1,A2,A3) ,axis=0)
+A1 = np.concatenate((np.flipud(np.fliplr(f_c)), np.flipud(f_c), np.flipud(np.fliplr(f_c))), axis=1)
+A2 = np.concatenate((np.fliplr(f_c),            f_c,            np.fliplr(f_c)), axis=1)
+A3 = np.concatenate((np.flipud(np.fliplr(f_c)), np.flipud(f_c), np.flipud(np.fliplr(f_c))), axis=1)
+f_proc = np.concatenate( (A1,A2,A3) ,axis=0)
 f_proc = f_proc[Nr-int((S-1)/2):2*Nr+int((S-1)/2), Nc-int((S-1)/2):2*Nc+int((S-1)/2),:]
 
-A_test = zeros([Nr,Nc])
+#f_proc = np.zeros([np.size(f_c,0)+S,np.size(f_c,1)+S,np.size(f_c,2)])
+#f_proc[int((S-1)/2):int((S-1)/2)+Nr,int((S-1)/2):int((S-1)/2)+Nc,:] = f_c.copy()
+
+A_test = np.zeros([Nr,Nc])
 #A_test2 = zeros([Nr,Nc])
-f_mv = zeros([S,S])
-K = floor(2*S*S/3)
+f_mv = np.zeros([S,S])
+K = np.floor(2*S*S/3)
 for k in range(Nr):
     leyend = 'Estimating Airlight: ' + str(int(100*(k+1)/Nr)) + '%'
     print(leyend)
@@ -117,26 +104,26 @@ for k in range(Nr):
         A_test[k,l] = u / (1 + v)
 print('Done!')
 
-x0,y0 = where( A_test == A_test.max())
-A = zeros(3)
+x0,y0 = np.where( A_test == A_test.max())
+A = np.zeros(3)
 A[0] = f_c[x0[0], y0[0],0]
 A[1] = f_c[x0[0], y0[0],1]
 A[2] = f_c[x0[0], y0[0],2]
 A_est = 0.2989*A[0] + 0.5870*A[1] + 0.1140*A[2]
 
-t_est = zeros([Nr,Nc])
-trans = zeros([Nr,Nc])
+t_est = np.zeros([Nr,Nc])
+trans = np.zeros([Nr,Nc])
 
-#PAR = [15, 0.01, 1.0, 4.] # Parameters for maguey
-#PAR = [9, 0.01, 10.0, 4.] # Parameters for flores
-PAR = [19, 0.6, 0.6, 6.] # Parameters for fuente
+PAR = [19, 0.01, 1.0, 8.] # Parameters for maguey
+#PAR = [5, 0.01, 10.0, 4.0] # Parameters for flores
+#PAR = [19, 0.6, 0.6, 6.] # Parameters for fuente
 
 S = PAR[0]      # Defines the size of sliding-window SxS
 w = PAR[1]      # Minimum allowed value of transmission
 j0 = PAR[2]     # Parameter for transmission estimation
 Kdiv = PAR[3]   # Parameter for caculation of K in adaptive neighborhoods
 
-y = zeros([Nr,Nc,Np])
+y = np.zeros([Nr,Nc,Np])
 K = S**2 - int((S**2)/Kdiv)
  
 for k in range(Nr):
@@ -162,17 +149,19 @@ for k in range(Nr):
         if t_est[k,l] < w:
             t_est[k,l] = w
 print('Done!')
-t_est = guidedFilter(uint8(f_c),uint8(255*t_est),30,0.05) / 255.0
-trans = denoise_tv_chambolle(t_est, weight=0.1, multichannel=False)
-#trans = denoise_bilateral(t_est, sigma_color=0.05, sigma_spatial=10, multichannel=False)
+
+#trans = denoise_tv_chambolle(t_est, weight=1000.0, multichannel=False)
+trans = guidedFilter(np.uint8(f_c),np.uint8(255*t_est),15,0.1) / 255.0
 
 y[:,:,0] = (f_c[:,:,0] - A[0]) / trans + A[0]
 y[:,:,1] = (f_c[:,:,1] - A[1]) / trans + A[1] 
 y[:,:,2] = (f_c[:,:,2] - A[2]) / trans + A[2]
 y[:,:,:] = abs(y[:,:,:])
+xs,ys,zs = np.where( y > 255 )
+y[xs,ys,zs] = 255.0
                     
 plt.subplot(121), plt.imshow(f_c/255.), plt.title('Hazy Image')
 plt.subplot(122), plt.imshow(y/255.), plt.title('Processed Image')
 
 
-imsave('%s_output.png' % (imgName), uint8(y))
+imsave('%s_output.png' % (imgName), np.uint8(y))
